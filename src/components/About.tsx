524c6fa76c18c48834bbf4e9193434fa0e9860fb
thaingan15122001 <thaingan15122001@gmail.com>
2026-07-27 12:13:55 +0700

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -114,7 +114,7 @@ export default function About() {
   }}
 >
   <source
-    src="https://cdn-cf-east.streamable.com/video/mp4/32bee0.mp4?Expires=1785387910717&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ&Signature=b4orH4PDMk-fyfC1Xk5IPw7KxEy1jfW-FN5ML25RyQtOcVgAvnUOR~lPuCHpHPWtaav82hixR1dJBW~0weJf6FwacfGyc8OcAVQcdPZxlM9EjB6o5nlkrpfnLx8JQT4rXuif93O2UNlOl9SIUguYQZbN~zSta8LIvGeJukQaev1z8XJYou8JyBPnyUTscxH1Zz1x5kSGnNgo1~NPOpV0K0zy0Fk3siXmbmTOn3MaWl80bFEQWLDKl5D6E1f6LCqz054jBJKgw9dxRLiMSe7rrJTC5-OReTv~tR4QbGEpkcPw9C2992l1WmZLgP5cCkt2Hs1iIjAV7YFhM3hUSWWxig__"
+    src="https://cdn-cf-east.streamable.com/video/mp4/zw7z5v.mp4?Expires=1785388406533&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ&Signature=bKGouIB2Hfo~OiP44DfN~IRpMdBGiBzuURTnxNdrhLruFXEUDltRuqYAh-aY2K0uzBktROxMQ2gPTACRXmqN8uGaRo-UPShZ1fK~GaohO4xoJUhYiaLw6TOaRpBNpmWT9igltE4Qmy9iF8JH2DXCyjDDR23Npa6nogCzcePlWDz3Cpiw1UKdk1SIpAHS2W~m2uoOr4zsC9ZGtY9wXYnJ1UriXD8M4wm0-IuUe8VlqTRThsMqIS5hHqKOBW5QQzj6Olzd~TOEZKh5A6hrCC3P-63beC4~vwLyCXp8pjpPjYlRVDiD5blrRb4SrZSk6R6sctbVzgy3ogIEGotWqvLu6w__"
     type="video/mp4"
   />
 </video>
